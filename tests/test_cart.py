86e49871986aee22f9d7ac61f6e2f4dba9056452
bianca1999 <61844717+bianca1999@users.noreply.github.com>
2025-06-26 20:32:30 +0800

    Parametrization for tests from test_cart.py
    Parametrization for tests from test_cart.py

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -4,24 +4,20 @@ from pages.cart_page import CartPage
 from pages.single_product_page import Product
 import pytest
 
-def test_add_products_to_cart(page):
+@pytest.mark.parametrize('product_id', [1, 2, 3, 4])
+def test_add_products_to_cart(page, product_id):
     home_page = HomePage(page)
     home_page.navigate()
     home_page.go_to_products_page()
 
     products_page = ProductsPage(page)
-    products_page.add_product_to_cart(1)
-    product1_price = products_page.get_product_price(1)
-
-    products_page.continue_shopping()
-
-    products_page.add_product_to_cart(2)
-    product2_price = products_page.get_product_price(2)
+    products_page.add_product_to_cart(product_id)
+    product_price = products_page.get_product_price(product_id)
     products_page.view_cart()
 
     cart_page = CartPage(page)
-    cart_page.test_if_a_product_is_in_the_cart(1, product1_price, "1", product1_price)
-    cart_page.test_if_a_product_is_in_the_cart(2, product2_price, "1", product2_price)
+    cart_page.test_if_a_product_is_in_the_cart(product_id, product_price, "1", product_price)
+    
 
 @pytest.mark.parametrize('product_id', [1, 2, 3, 4])
 def test_verify_product_quantity_in_cart(page, product_id):
@@ -43,17 +39,16 @@ def test_verify_product_quantity_in_cart(page, product_id):
     cart_page = CartPage(page)
     cart_page.test_total_price_for_a_product(product_id, total_price)
 
-def test_remove_product_from_cart(page):
+@pytest.mark.parametrize('product_id', [1, 2, 3, 4])
+def test_remove_product_from_cart(page, product_id):
     home_page = HomePage(page)
     home_page.navigate()
     home_page.go_to_products_page()
 
     products_page = ProductsPage(page)
-    products_page.add_product_to_cart(1)
-    products_page.continue_shopping()
-    products_page.add_product_to_cart(2)
+    products_page.add_product_to_cart(product_id)
     products_page.view_cart()
 
     cart_page = CartPage(page)
-    cart_page.remove_a_product_from_the_cart(1)
+    cart_page.remove_a_product_from_the_cart(product_id)
     
